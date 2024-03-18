a9d063c6ecc84fb3cebd34771d1a668b1d64bb67
Banjo Obayomi <bobayomi@amazon.com>
2024-03-18 10:57:02 -0400

    update understanding

--- full_code/image_understanding_st_full.py
+++ full_code/image_understanding_st_full.py
@@ -11,28 +11,6 @@ st.subheader("Image Understanding Demo")
 
 REGION = "us-west-2"
 
-# List of Stable Diffusion Preset Styles
-sd_presets = [
-    "None",
-    "3d-model",
-    "analog-film",
-    "anime",
-    "cinematic",
-    "comic-book",
-    "digital-art",
-    "enhance",
-    "fantasy-art",
-    "isometric",
-    "line-art",
-    "low-poly",
-    "modeling-compound",
-    "neon-punk",
-    "origami",
-    "photographic",
-    "pixel-art",
-    "tile-texture",
-]
-
 # Define bedrock
 bedrock_runtime = boto3.client(
     service_name="bedrock-runtime",
@@ -40,7 +18,7 @@ bedrock_runtime = boto3.client(
 )
 
 
-def call_claude_sonet(base64_string):
+def call_claude_sonnet(base64_string):
 
     prompt_config = {
         "anthropic_version": "bedrock-2023-05-31",
@@ -78,91 +56,12 @@ def call_claude_sonet(base64_string):
     return results
 
 
-# Bedrock api call to stable diffusion
-def generate_image_sd(text, style):
-    """
-    Purpose:
-        Uses Bedrock API to generate an Image
-    Args/Requests:
-         text: Prompt
-         style: style for image
-    Return:
-        image: base64 string of image
-    """
-    body = {
-        "text_prompts": [{"text": text}],
-        "cfg_scale": 10,
-        "seed": 0,
-        "steps": 50,
-        "style_preset": style,
-    }
-
-    if style == "None":
-        del body["style_preset"]
-
-    body = json.dumps(body)
-
-    modelId = "stability.stable-diffusion-xl"
-    accept = "application/json"
-    contentType = "application/json"
-
-    response = bedrock_runtime.invoke_model(
-        body=body, modelId=modelId, accept=accept, contentType=contentType
-    )
-    response_body = json.loads(response.get("body").read())
-
-    results = response_body.get("artifacts")[0].get("base64")
-    return results
-
 
 # function to convert PIL image to base64 string
 def pil_to_base64(image, format="png"):
     with io.BytesIO() as buffer:
         image.save(buffer, format)
         return base64.b64encode(buffer.getvalue()).decode()
-
-
-def convert_base64_to_image(base64_string):
-    img_data = base64.b64decode(base64_string)
-    img = Image.open(io.BytesIO(img_data))
-    return img
-
-
-def generate_image_titan(text):
-    """
-    Purpose:
-        Uses Bedrock API to generate an Image using Titan
-    Args/Requests:
-         text: Prompt
-    Return:
-        image: base64 string of image
-    """
-    body = {
-        "textToImageParams": {"text": text},
-        "taskType": "TEXT_IMAGE",
-        "imageGenerationConfig": {
-            "cfgScale": 10,
-            "seed": 0,
-            "quality": "standard",
-            "width": 512,
-            "height": 512,
-            "numberOfImages": 1,
-        },
-    }
-
-    body = json.dumps(body)
-
-    modelId = "amazon.titan-image-generator-v1"
-    accept = "application/json"
-    contentType = "application/json"
-
-    response = bedrock_runtime.invoke_model(
-        body=body, modelId=modelId, accept=accept, contentType=contentType
-    )
-    response_body = json.loads(response.get("body").read())
-
-    results = response_body.get("images")[0]
-    return results
 
 
 # Streamlit file uploader for only for images
@@ -179,7 +78,7 @@ if user_image is not None:
     if col1.button("Describe Image"):
         base64_string = pil_to_base64(user_image)
 
-        desc_image = call_claude_sonet(base64_string)
+        desc_image = call_claude_sonnet(base64_string)
         col2.write(desc_image)
 else:
     col2.write("No image uploaded")
